810495622f71bcb3e81896553a5b538b5b0e0548
Golluméo <p.mauriello54@gmail.com>
2025-01-13 23:17:04 +0100

    chore(deps): install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{vue,js,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Montserrat Alternates', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+};
+
+
