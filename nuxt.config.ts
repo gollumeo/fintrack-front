003d199a11a5147060350bab6c106cac8078f740
Golluméo <p.mauriello54@gmail.com>
2025-01-14 23:55:32 +0100

    chore(deps): config sanctum (back) + $fetch using credentials

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
   runtimeConfig: {
     public: {
       apiBase: "http://fintrack-api.test/api/",
--- plugins/fetch.config.ts
+++ plugins/fetch.config.ts
@@ -5,9 +5,21 @@ export default defineNuxtPlugin(() => {
                 return $fetch(url, {
                     baseURL: useRuntimeConfig().public.apiBase, // Définit l'URL de base globale
                     credentials: 'include', // Inclut automatiquement les cookies
+                    headers: {
+                        'X-XSRF-TOKEN': getCookieValue('XSRF-TOKEN') || '',
+                    },
                     ...options, // Permet de personnaliser les requêtes si besoin
                 });
             },
         },
     };
 });
+
+function getCookieValue(name: string): string | undefined {
+    const cookies = document.cookie.split('; ').reduce((acc, cookie) => {
+        const [key, value] = cookie.split('=');
+        acc[key] = value;
+        return acc;
+    }, {} as Record<string, string>);
+    return cookies[name];
+}
